3611d7c9f759da6cbd92f6a069d13af3276ea5d8
Tobias FS <tobias.fs404@gmail.com>
2026-01-23 12:16:51 -0300

    fix: corrige  path do Dotenv para localizar arquivo .env corretamente

--- src/infra/banco/conexao.php
+++ src/infra/banco/conexao.php
@@ -1,11 +1,11 @@
 <?php
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../');
 $dotenv->load();
 
 function criarConexao() {
     return new PDO( 
-        "mysql:dbname={$_ENV['DB_NAME']};host={$_ENV['DB_HOST']};charset=utf8",
+        "mysql:dbname=chef-book;host=127.0.0.1;charset=utf8",
         $_ENV[ 'DB_USER' ],
         $_ENV[ 'DB_PASS' ],
         [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ] );
