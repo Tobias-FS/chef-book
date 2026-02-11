af91c89de1f715e43e5e8ff817fa21e835f048ab
Tobias Ferreira da Silva <tobias.fs404@gmail.com>
2026-02-11 16:58:59 -0300

    chore: adiciona configuração de ambiente com docker e mise

--- phinx.php
+++ phinx.php
@@ -27,7 +27,7 @@ return
             'name' => $_ENV[ 'DB_NAME' ],
             'user' => $_ENV[ 'DB_USER' ],
             'pass' => $_ENV[ 'DB_PASS' ],
-            'port' => '3306',
+            'port' => $_ENV[ 'DB_PORT' ],
             'charset' => 'utf8',
         ],
         'testing' => [
