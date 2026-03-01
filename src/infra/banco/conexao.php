078801c7a05fa400cd4a71e91719658404698f80
Tobias Ferreira da Silva <tobias.fs404@gmail.com>
2026-03-01 18:06:08 -0300

    refactor: usar variáveis de ambiente na conexão PDO

--- src/infra/banco/conexao.php
+++ src/infra/banco/conexao.php
@@ -4,8 +4,13 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../');
 $dotenv->load();
 
 function criarConexao() {
+    $dsn = "mysql:host=" . $_ENV['DB_HOST'] . 
+               ";port=" . $_ENV['DB_PORT'] . 
+               ";dbname=" . $_ENV['DB_NAME'] . 
+               ";charset=utf8";
+    
     return new PDO( 
-        "mysql:dbname=chef-book;host=127.0.0.1;charset=utf8",
+        $dsn,
         $_ENV[ 'DB_USER' ],
         $_ENV[ 'DB_PASS' ],
         [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ] );
